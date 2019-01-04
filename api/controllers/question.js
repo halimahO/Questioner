170d1f95cc02b5d47b794b9290cd17e5c414bf4c
Halimah <haleemaholadosu@gmail.com>
2019-01-04 18:36:36 +0100

    Create an endpoint for - downvote question

--- api/controllers/question.js
+++ api/controllers/question.js
@@ -41,6 +41,27 @@ const Question = {
       }],
     });
   },
+
+  // Controller for downvote a question
+  downvote(req, res) {
+    const question = questionModel.fetchOneQuestion(req.params.id);
+    if (!question) {
+      return res.status(404).send({
+        message: 'Question not found',
+      });
+    }
+    const questionVote = questionModel.downvote(req.params.id);
+    return res.status(200).send({
+      status: 200,
+      data: [{
+        meetup: questionVote.meetup,
+        title: questionVote.title,
+        body: questionVote.body,
+        votes: questionVote.votes,
+
+      }],
+    });
+  },
 };
 
 export default Question;
