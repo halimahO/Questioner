5fed36e739d67de15f567c9986ca80f81b3424b6
Halimah <haleemaholadosu@gmail.com>
2019-01-04 18:38:46 +0100

    Fix typo in upvote route

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,6 +9,6 @@ routes.get('/api/v1/meetups/upcoming', meetup.getUpcoming);
 routes.get('/api/v1/meetups/:id', meetup.getOneMeetup);
 routes.get('/api/v1/meetups', meetup.getAllMeetup);
 routes.post('/api/v1/questions', question.createQuestion);
-routes.patch('/api/v1/questions/:id/upvotes', question.upvote);
+routes.patch('/api/v1/questions/:id/upvote', question.upvote);
 
 export default routes;
